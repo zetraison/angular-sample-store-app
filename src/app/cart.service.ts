26bdc2d75fa56290b8924cb85a28807c9253f3c3
zetraison <alexse790@mail.ru>
2020-09-26 23:58:53 +0500

    Add HTTP client to application

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-
   items = [];
+
+  constructor(
+    private http: HttpClient
+  ) {}
 
   addToCart(product) {
     this.items.push(product);
@@ -19,5 +23,9 @@ export class CartService {
     this.items = [];
     return this.items;
   }
+
+  getShippingPrices() {
+    return this.http.get('/assets/shipping.json');
+  }  
 
 }
